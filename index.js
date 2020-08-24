6e0fd99e0c937ca1a43dc2181120fc2decdadcbf
Jack Magnus <jack.magnus@jadomag.com>
2020-08-24 18:03:08 +0200

    Site cleanup

--- index.js
+++ index.js
@@ -1,46 +1,47 @@
 document.querySelectorAll('.years').forEach(function(node) {
-    const now = new Date(),
-          date = new Date(node.getAttribute('data-date'));
+  const now = new Date()
+  const date = new Date(node.getAttribute('data-date'))
+  const millisInYear = 1000 * 60 * 60 * 24 * 365
+  const years = (now - date) / millisInYear
+  node.textContent = Math.floor(years)
+})
 
-    let duration = now.getFullYear() - date.getFullYear();
-
-    date.setYear(now.getYear());
-
-    if (date < now) {
-        duration--;
-    }
-
-    node.textContent = duration;
-});
+document.querySelectorAll('.js-only').forEach(function(node) {
+  node.style.display = 'initial'
+})
 
 // ===============
 
 function changeSection() {
-    const id = window.location.hash.substr(1) || 'introduction';
+  for (const node of document.getElementsByTagName('section')) {
+    node.style.display = 'none'
+  }
 
-    let section = document.getElementById(id);
+  let section
+  if (window.location.hash) {
+    const id = window.location.hash.substr(1) || 'introduction'
+    section = document.getElementById(id)
+  }
 
-    if (section && section.nodeName === 'SECTION') {
-        document.querySelectorAll('section').forEach(function(node) {
-            node.style.display = null;
-        });
+  if (section && section.nodeName === 'SECTION') {
+    document.querySelectorAll('header nav a').forEach(function(node) {
+      node.classList.remove('active')
+    })
 
-        document.querySelectorAll('header nav a').forEach(function(node) {
-            node.classList.remove('active');
-        });
+    section.style.display = 'block'
 
-        section.style.display = 'block';
-        document.getElementById('nav-' + id).classList.add('active');
-    }
+    const nav = document.getElementById('nav-' + section.id)
+    nav.classList.add('active')
+  } else {
+    window.location.hash = '#introduction'
+    changeSection()
+  }
 }
+
+changeSection()
 
 // ===============
 
 window.onload = function() {
-    if (!window.location.hash) {
-        window.location = "#introduction";
-    }
-
-    changeSection();
-    window.addEventListener('hashchange', changeSection);
-};
+  window.addEventListener('hashchange', changeSection)
+}
